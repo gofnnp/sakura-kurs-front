9b2bc20c6965d8807228ecdd5ca1fa35858024b9
Kataev Denis <kataev@hlcompany.ru>
2022-09-07 17:49:59 +0400

    dev #12401 правки по верстке

---
+++ src/assets/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCujHg9GtN8Uxi-JcCN8zggvXlfNQRKc04",
+    authDomain: "push-notification-test2-56dac.firebaseapp.com",
+    projectId: "push-notification-test2-56dac",
+    storageBucket: "push-notification-test2-56dac.appspot.com",
+    messagingSenderId: "1004369687552",
+    appId: "1:1004369687552:web:a6cc20625e05520a37d4e5"
+});
+
+const messaging = firebase.messaging();
