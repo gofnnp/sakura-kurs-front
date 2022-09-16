aced65293d7e28dc93f8c3b12ed6f4cd5047bb32
Kataev Denis <kataev@hlcompany.ru>
2022-09-16 12:01:56 +0400

    dev #12401 правки

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   appWPEndpoint: 'http://213.239.210.240:4500/wp-json/woofood/v1/',
   hasBonusProgram: true,
   systemId: 'g6zyv8tj53w28ov7cl',
-  defaultUrl: 'lk.crm4retail.ru/fashion-logica',
+  defaultUrl: 'https://lk.crm4retail.ru/fashion-logica',
   firebase: {
     apiKey: "AIzaSyCujHg9GtN8Uxi-JcCN8zggvXlfNQRKc04",
     authDomain: "push-notification-test2-56dac.firebaseapp.com",
