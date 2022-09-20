534fdda325d0acb311ab08f539940fe1e4d1c06a
gofnnp <fbicha1234@gmail.com>
2022-09-20 12:50:42 +0400

    dev #12401 инструкция для скачивания на айфон

--- src/app/components/footer-buttons/footer-buttons.component.ts
+++ src/app/components/footer-buttons/footer-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-footer-buttons',
@@ -11,15 +12,30 @@ export class FooterButtonsComponent implements OnInit {
   @Input() isPermissionNotifications!: boolean;
   @Output() downloadingPWA = new EventEmitter<null>();
   @Output() requestingPermission = new EventEmitter<null>();
+  public deviceType: 'ios' | 'android' | null = null;
 
   constructor(
-
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
+    this.getTypeDevice()
+  }
+
+  getTypeDevice() {
+    const userAgent = window.navigator.userAgent.toLowerCase()
+    const ios = /iphone|ipod|ipad/.test( userAgent );
+    this.deviceType = ios ? 'ios' : 'android'
   }
 
   downloadPWA() {
+    if (this.deviceType === 'ios') {
+      this.messageService.add({
+        severity: 'custom',
+        summary: "Для установки нажмите на кнопку поделиться в Вашем браузере и выберите пункт 'Добавить на главный экран'",
+      });
+      return
+    }
     this.downloadingPWA.emit(null)
   }
 
