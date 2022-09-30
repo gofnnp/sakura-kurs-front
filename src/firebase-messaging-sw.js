4414480d6ee31f7bc23c1efc362823859776024c
gofnnp <fbicha1234@gmail.com>
2022-09-30 13:02:57 +0400

    dev #12425 Поправил сохранения токенов для пушей

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.4.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.4.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-messaging.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyCnKvln5itnrBj62POCPHxshAN_Vmd0zds",
